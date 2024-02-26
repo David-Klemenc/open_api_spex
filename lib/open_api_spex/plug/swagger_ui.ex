f5438ff28ed761a3802dcec847b13892c03d9fb9
David-Klemenc <44705886+David-Klemenc@users.noreply.github.com>
2024-02-26 16:23:33 +0100

    add dark mode media css

--- lib/open_api_spex/plug/swagger_ui.ex
+++ lib/open_api_spex/plug/swagger_ui.ex
@@ -60,6 +60,14 @@ defmodule OpenApiSpex.Plug.SwaggerUI do
           margin:0;
           background: #fafafa;
         }
+        @media (prefers-color-scheme: dark) {
+          .swagger-ui {
+              filter: invert(88%) hue-rotate(180deg);
+          }
+          .swagger-ui .microlight {
+              filter: invert(100%) hue-rotate(180deg);
+          }
+        }
       </style>
     </head>
 
